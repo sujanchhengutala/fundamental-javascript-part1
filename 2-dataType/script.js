7989dca0fcf7daf068e6e363c814e6c9221712b4
sujanchhengutala9@gmail.com <sujanchhengutala9@gmail.com>
2021-06-16 13:36:50 +0545

    dataType

---
+++ 2-dataType/script.js
@@ -0,0 +1,29 @@
+//1. string
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+
+//-------------- 1)example of string------------------
+
+//let myName=`sujan chhengutala`;
+//console.log(typeof myName);
+
+//---------------2)example of number-------------------
+//const dateofBirth=1999-08-21;
+//console.log(typeof dateofBirth);
+
+//----------------3)Boolean example--------------------
+//let imTwentyone=true;
+//console.log(typeof imTwentyone);
+
+//-----------------4)datatypeof undefine,null,symbol---------------=
+//console.log(typeof undefined);
+//console.log(typeof null);
+//console.log(typeof symbol);
+
+//----------------------------------------------------------------------------
+
+
