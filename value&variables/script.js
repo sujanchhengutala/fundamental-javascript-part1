81f73729669f434e192a65917bf17b3390537875
sujanchhengutala9@gmail.com <sujanchhengutala9@gmail.com>
2021-06-16 13:12:42 +0545

    value and variable

---
+++ value&variables/script.js
@@ -0,0 +1,13 @@
+const Country = "Nepal";
+console.log(Country);
+// alert(Country);  //show the country content
+
+const continent = "Asia";
+console.log(continent);
+// alert(continent); // show the continent contents
+
+let population;
+population = 29626628;
+console.log(population);
+// alert(population);  // show population
+
